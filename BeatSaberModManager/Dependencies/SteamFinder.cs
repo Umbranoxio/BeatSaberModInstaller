e95d9745b3630b373b85e29695a691c0b2f20822
Unknown <itsumbra@protonmail.com>
2018-08-04 16:33:56 +1000

    Moved SteamFinder

--- BeatSaberModManager/Dependencies/SteamFinder.cs
+++ BeatSaberModManager/Dependencies/SteamFinder.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace BeatSaberModManager.Core
+namespace BeatSaberModManager.Dependencies
 {
     /// <summary>
     /// Steam installation path and Steam games folder finder.
