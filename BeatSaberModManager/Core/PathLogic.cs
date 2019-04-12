ae95783672c8b8707163edf3ed73518a302ea508
megalon <megalongaming@gmail.com>
2019-04-12 15:11:45 -0700

    Set path to saved install path, if it is valid

--- BeatSaberModManager/Core/PathLogic.cs
+++ BeatSaberModManager/Core/PathLogic.cs
@@ -20,6 +20,16 @@ namespace BeatSaberModManager.Core
 
         public string GetInstallationPath()
         {
+            // Check if install path is already saved
+            if (Properties.Settings.Default.InstallPath != null)
+            {
+                if (File.Exists(Path.Combine(Properties.Settings.Default.InstallPath, AppFileName)))
+                {
+                    installPath = Properties.Settings.Default.InstallPath;
+                    return installPath;
+                }
+            }
+
             string steam = GetSteamLocation();
             if (steam != null)
             {
