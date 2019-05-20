6604352d2b3ae8b3a5d29b9b3382da960aa40c39
megalon <megalongaming@gmail.com>
2019-05-20 15:15:14 -0700

    WIP announcement form

---
+++ BeatSaberModManager/Forms/FormAnnouncement.cs
@@ -0,0 +1,26 @@
+﻿using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BeatSaberModManager.Forms
+{
+    public partial class FormAnnouncement : MaterialForm
+    {
+        public FormAnnouncement()
+        {
+            InitializeComponent();
+        }
+
+        private void FormAnnouncement_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
